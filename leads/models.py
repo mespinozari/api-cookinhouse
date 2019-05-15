d3482da6b74e3b41811f98ea631ad1e93fd1dbb8
Manuel <rieraes@gmail.com>
2019-05-15 17:46:58 -0500

    api-validado

--- leads/models.py
+++ leads/models.py
@@ -5,3 +5,6 @@ class Lead(models.Model):
     email = models.EmailField(max_length=100, unique=True)
     message = models.CharField(max_length=500, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField()
+    
+
